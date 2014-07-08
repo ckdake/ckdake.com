ff537af7c546e8ad91ea6cedd24602eac542eb59
Chris Kelly <ckdake@ckdake.com>
2014-07-08 18:02:09 -0400

    Adds sitemap etc

--- content/index.md
+++ content/index.md
@@ -2,5 +2,5 @@
 title: home
 ---
 
-A Brand New ckdake.com
+Hello!
 ====================
--- lib/default.rb
+++ lib/default.rb
@@ -4,3 +4,27 @@ include Nanoc3::Helpers::Blogging
 include Nanoc3::Helpers::Tagging
 include Nanoc3::Helpers::Rendering
 include Nanoc3::Helpers::LinkTo
+
+def previous_link
+  prev = sorted_articles.index(@item) + 1
+  prev_article = sorted_articles[prev]
+  if prev_article.nil?
+    ''
+  else
+    title = prev_article[:title]
+    html = "&larr; Previous"
+    link_to(html, prev_article.reps[0], :class => "previous", :title => title)
+  end
+end
+
+def next_link
+  nxt = sorted_articles.index(@item) - 1
+  if nxt < 0
+    ''
+  else
+    post = sorted_articles[nxt]
+    title = post[:title]
+    html = "Next &rarr;"
+    link_to(html, post.reps[0], :class => "next", :title => title)
+  end
+end
