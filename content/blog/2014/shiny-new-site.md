92e0f6e1a6d92873aa3bd8cf798d12f6d93b0998
Chris Kelly <ckdake@ckdake.com>
2014-07-09 00:25:29 -0400

    Videos and fonts

---
+++ content/blog/2014/shiny-new-site.md
@@ -0,0 +1,14 @@
+---
+title: A Shiny New Website
+created_at: 2014-07-09 14:14:37 -0400
+kind: article
+---
+
+1. Why (cost, disk space, ability to experiment, g2 choking on uploads, git+ruby, static)
+1. Migrating Photos To Flickr
+1. Migrating Blog Posts to nanoc
+1. Picking/Getting set up on nanoc (Gemfile)
+1. Getting Flickr on nanoc
+1. Getting vimeo on nanoc
+1. Getting the URLs I like on nanoc
+1. Site Search
---
+++ lib/vimeo.rb
@@ -0,0 +1,41 @@
+require 'vimeo'
+
+$videos = nil
+
+def recent_vimeo_videos_embed_html(count)
+  html = ''
+  $videos ||= Vimeo::Simple::User.videos("ckdake")
+  (0..(count.to_i - 1)).each do |i|
+    html = html + vimeo_video_to_embed_html(
+      {
+        id: $videos[i]['id'],
+        width: $videos[i]['width'],
+        height: $videos[i]['height']
+      }
+    )
+  end
+  html
+end
+
+def recent_vimeo_videos_link_html(count)
+  html = ''
+  $videos ||= Vimeo::Simple::User.videos("ckdake")
+  (0..(count.to_i - 1)).each do |i|
+    html = html + vimeo_video_to_link_html(
+      {
+        title: $videos[i]['title'],
+        thumbnail: $videos[i]['thumbnail_medium'],
+        url: $videos[i]['url']
+      }
+    )
+  end
+  html
+end
+
+def vimeo_video_to_link_html(info)
+  "<div><a href='#{info[:url]}'><img src='#{info[:thumbnail]}'><br />#{info[:title]}</div>"
+end
+
+def vimeo_video_to_embed_html(info)
+  "<iframe src='//player.vimeo.com/video/#{info[:id]}' width='#{info[:width].to_i/4}' height='#{info[:height].to_i/4}' frameborder='0' webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>"
+end
